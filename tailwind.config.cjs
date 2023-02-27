97564c54fae9e1f826ce43a278cf06128d2f2bb1
Rodgers M Gitau <rodgers.gitau@gmail.com>
2023-02-27 19:31:52 +0300

    feat: setup tailwind & navbar

---
+++ tailwind.config.cjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        black: {
+          50: "#E8E8E8",
+          100: "#D4D4D4",
+          200: "#A6A6A6",
+          300: "#7A7A7A",
+          400: "#4D4D4D",
+          500: "#212121",
+          600: "#1A1A1A",
+          700: "#141414",
+          800: "#0D0D0D",
+          900: "#080808",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
